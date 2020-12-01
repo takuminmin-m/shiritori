8e44b5f805c93635fcf578dffb6e76f38b4c88c3
takuminmin-m <takuminmin0718@gmail.com>
2020-12-01 22:27:46 +0900

    generate word model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_054503) do
+ActiveRecord::Schema.define(version: 2020_12_01_132547) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2020_11_28_054503) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
