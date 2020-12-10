de387538460b48103b3de30714ef7e7b6bac429f
takuminmin-m <takuminmin0718@gmail.com>
2020-12-10 18:32:45 +0900

    set letter_operner_web on development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  ActionMailer::Base.delivery_method = :letter_opener_web
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :rooms, only: [:show, :index, :create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # setting for letter_opener_web gem
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
