468196903f7cfa57e35a8b804acfde0ed3ba739e
takuminmin-m <takuminmin0718@gmail.com>
2020-12-15 23:10:09 +0900

    add the rule of shiritori

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    room = Room.find(params[:room])
+    room = Room.find(params["room"])
     stream_for room
   end
 
@@ -9,6 +9,18 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    Word.create! content: data["word"], user_id: current_user.id, room_id: params["room"]
+    set_room
+    if !(@room.words.exists?)
+      Word.create! content: data["word"], user_id: current_user.id, room_id: params["room"]
+    elsif @room.words.last.content[-1] == data["word"][0]
+      Word.create! content: data["word"], user_id: current_user.id, room_id: params["room"]
+    end
+  end
+
+
+  private
+
+  def set_room
+    @room = Room.find(params["room"])
   end
 end
