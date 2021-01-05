36dd5bbd78e178207ec11ad4137151bc2ecb0992
takuminmin-m <takuminmin0718@gmail.com>
2021-01-05 21:23:35 +0900

    ひらがなのみしか使用できないように変更

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -10,14 +10,18 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
     set_room
-    if data["word"][-1] == "ん"
-      send_message("語尾に「ん」がついてはいけません")
-    elsif !(@room.words.exists?)
-      Word.create! content: data["word"], user_id: current_user.id, room_id: params["room"]
-    elsif @room.words.last.content[-1] == data["word"][0]
-      Word.create! content: data["word"], user_id: current_user.id, room_id: params["room"]
+    if !(data["word"] =~ /\p{Hiragana}/)
+      send_message "ひらがなのみを使用してください"
+    elsif data["word"][-1] == "ん"
+      send_message "語尾に「ん」がついてはいけません"
+    elsif @room.words.exists?
+      if @room.words.last.content[-1] != data["word"][0]
+        send_message "前の言葉の最後の文字ととこの言葉の最初の文字が一致していません"
+      else
+        post_word data["word"], params["room"]
+      end
     else
-      send_message("この言葉はルールに適合していません")
+      post_word data["word"], params["room"]
     end
   end
 
@@ -30,5 +34,9 @@ class RoomChannel < ApplicationCable::Channel
 
   def send_message(message)
     UserMessageBroadcastJob.perform_later message, current_user
+  end
+
+  def post_word(word, room_id)
+    Word.create! content: word, user_id: current_user.id, room_id: room_id
   end
 end
